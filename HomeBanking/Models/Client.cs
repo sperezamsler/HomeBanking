2eb156bdfe3e5324e7b297ec6e927d6564b7f968
spamsler <sergioperezamsler@gmail.com>
2023-07-25 10:40:50 -0300

    Agregar archivos de proyecto.

---
+++ HomeBanking/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace HomeBanking.Models
+{
+    public class Client
+    {
+        //escribir prop es un atajo para crear atributos
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<Account> Accounts { get; set; }
+    }
+}
